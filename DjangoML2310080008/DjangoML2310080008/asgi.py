d944c17a9a3baddd16c7b7fa9e5c334a523907e5
bhuvanabhagi <bhagibhuvaneswari@gmail.com>
2024-11-29 22:55:32 +0530

    initial commit

---
+++ DjangoML2310080008/DjangoML2310080008/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for DjangoML2310080008 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoML2310080008.settings')
+
+application = get_asgi_application()
